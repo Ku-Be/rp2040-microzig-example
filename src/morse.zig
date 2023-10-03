9a12a5a9c0e622faf60aa255ca1f98aed4cdf8b8
Ku-Be <>
2023-10-03 18:30:05 +0200

    Morse code boolean slices are now generated from strings at comptime.

--- src/morse.zig
+++ src/morse.zig
@@ -15,12 +15,15 @@ const pin_config = rp2040.pins.GlobalConfiguration{
 };
 var p: rp2040.pins.Pins(pin_config) = undefined;
 
-const myimportedfunction = @extern(*const fn(u32,u32) callconv(.C) u32,.{.name="myfunction"});
+const myimportedfunction = @extern(*const fn (u32, u32) callconv(.C) u32, .{ .name = "myfunction" });
 
-// parameter: die lookuptabelle *u16, entweder data oder func, und der buchstabencode als u32
-var optional_rom_table_lookup: ?*const fn (*u16,u32) callconv(.C) u32 = @ptrFromInt(0);
-// parameter: led-pin-bitmaske u32, disable-interface-mask
-var optional_reset_to_usb: ?*const fn(u32,u32) callconv(.C) void = @ptrFromInt(0);
+
+// ptrFromInt(0x18) must be a *u16, as 16-bit pointers are used in the bootrom.
+// parameters: rom lookup table *u16, either data or func, letter code as u32
+var rom_table_lookup: ?*const fn (*u16, u32) callconv(.C) u32 = undefined;
+const rom_table_lookup_location = 0x18;
+// parameters: led-pin-bitmask u32, disable-interface-mask
+var optional_reset_to_usb: ?*const fn (u32, u32) callconv(.C) void = undefined;
 
 pub fn main() void {
     const pins = pin_config.apply();
@@ -29,49 +32,19 @@ pub fn main() void {
     pins.pwmpin.slice().set_clk_div(16, 0);
     pins.led.put(0);
     p = pins;
-    var result: u32 = myimportedfunction(1,2);
+    var result: u32 = myimportedfunction(1, 2);
     for (0..result) |_| {
-        blinkstr("SOS");
+        blinkstr("HOHO");
         pins.pwmpin.slice().disable();
     }
-		
-		// veraltet, in einer früheren version von zig gab es @intToPtr
-		//go to usb boot
-    //const find_bootrom_data = @intToPtr(*const fn (*u16, u32) u32, @intToPtr(*u16, 0x0000_0018).*);
-    //const reset_to_usb: u32 = find_bootrom_data(@intToPtr(*u16, @intToPtr(*u16, 0x0000_0014).*), (('B' << 8) | 'U'));
-    //@intToPtr(*const fn (u32, u32) void, reset_to_usb)(0, 0);
-    
-		// falsch:
-		// const rom_table_lookup: *const fn (*u16, u32) callconv(.C) u32 = @as(*(*const fn (*u16, u32) callconv(.C) u32),@ptrFromInt(0x0000_0018)).*;
-    // const reset_to_usb_location: u32 = rom_table_lookup(@as(**u16,@ptrFromInt(0x0000_0014)).*, (@as(u32,('B' << 8) | 'U')));
-    // const reset_to_usb: *const fn(u32,u32) callconv(.C) void = @ptrFromInt(reset_to_usb_location);
-		
-    // richtig:
-		const rom_func_table: *u16 = @ptrFromInt(@as(*u16,@ptrFromInt(0x0000_0014)).*);
-		// const rom_data_table: *u16 = @ptrFromInt(@as(*u16,@ptrFromInt(0x0000_0016)).*);
-		
-		// richtig:
-		// Ich habe 0x18, das ist die addresse von einem u16.
-		// Also ist @ptrFromInt(0x18) ein *u16.
-		// Also enthalten 0x18 und 0x19 die bytes.
-		// Dereferenziert ist das dann ein u16.
-		// Dieser u16 ist wiederum ein pointer auf eine Funktion.
-		// Es ist wichtig zu beachten dass @ptrFromInt(0x18) NICHT ein *u32 ist,
-		// denn sonst werden 0x18, 0x19, 0x20 und 0x21 als die bytes interpretiert,
-		// die dann wiederum ein pointer sein sollen.
-		// const rom_table_lookup: *const fn (*u16,u32) callconv(.C) u32 = @ptrFromInt(@as(*u16,@ptrFromInt(0x18)).*);
-		// const reset_to_usb_location: u32 = rom_table_lookup(rom_func_table, ('B' << 8) | 'U');
-		// const reset_to_usb: *const fn(u32,u32) callconv(.C) void = @ptrFromInt(reset_to_usb_location);
-		// reset_to_usb((1<<25),0);
-		
-		// auch richtig:
-		// wenn man die function pointer global definieren möchte, müssen sie uninitialisiert bleiben.
-		// das ist aber verboten, also müssen sie optional sein.
-		// weil sie später verändert werden, sind sie außerdem nicht const.
-		// außerdem müssen sie beim aufrufen mit .? unoptionalisiert werden.
-		optional_rom_table_lookup = @ptrFromInt(@as(*u16,@ptrFromInt(0x18)).*);
-		optional_reset_to_usb = @ptrFromInt(optional_rom_table_lookup.?(rom_func_table, ('B' << 8) | 'U'));
-		optional_reset_to_usb.?((1<<25),0);
+	// bootrom function lookup
+	// the pointers are dereferenced here, which may only happen at runtime.
+	// the results should not differ on rp2040 chips with the same hardware version, so could be hard-coded.
+    const rom_func_table: *u16 = @ptrFromInt(@as(*u16, @ptrFromInt(0x0000_0014)).*);
+    // unused const rom_data_table: *u16 = @ptrFromInt(@as(*u16,@ptrFromInt(0x0000_0016)).*);
+    rom_table_lookup = @ptrFromInt(@as(*u16, @ptrFromInt(rom_table_lookup_location)).*);
+    optional_reset_to_usb = @ptrFromInt(rom_table_lookup.?(rom_func_table, ('B' << 8) | 'U'));
+    optional_reset_to_usb.?((1 << 25), 0);
 }
 
 const baseduration: u32 = 140 / 4 * 2;
@@ -95,7 +68,6 @@ const duration = enum {
 fn blinkstr(s: []const u8) void {
     for (s) |v| {
         if (v == ' ') {
-            //blinkchr(26);
             wait(duration.inter_word);
         } else {
             blinkchr(v - 65);
@@ -134,139 +106,34 @@ fn blinkchr(c: u8) void {
     wait(duration.inter_letter);
 }
 
-const M = [_][]const bool{
-    &[_]bool{
-        false,
-        true,
-    },
-    &[_]bool{
-        true,
-        false,
-        false,
-        false,
-    },
-    &[_]bool{
-        true,
-        false,
-        true,
-        false,
-    },
-    &[_]bool{
-        true,
-        false,
-        false,
-    },
-    &[_]bool{
-        false,
-    },
-    &[_]bool{
-        false,
-        false,
-        true,
-        false,
-    },
-    &[_]bool{
-        true,
-        true,
-        false,
-    },
-    &[_]bool{
-        false,
-        false,
-        false,
-        false,
-    },
-    &[_]bool{
-        false,
-        false,
-    },
-    &[_]bool{
-        false,
-        true,
-        true,
-        true,
-    },
-    &[_]bool{
-        true,
-        false,
-        true,
-    },
-    &[_]bool{
-        false,
-        true,
-        false,
-        false,
-    },
-    &[_]bool{
-        true,
-        true,
-    },
-    &[_]bool{
-        true,
-        false,
-    },
-    &[_]bool{
-        true,
-        true,
-        true,
-    },
-    &[_]bool{
-        false,
-        true,
-        true,
-        false,
-    },
-    &[_]bool{
-        true,
-        true,
-        false,
-        true,
-    },
-    &[_]bool{
-        false,
-        true,
-        false,
-    },
-    &[_]bool{
-        false,
-        false,
-        false,
-    },
-    &[_]bool{
-        true,
-    },
-    &[_]bool{
-        false,
-        false,
-        true,
-    },
-    &[_]bool{
-        false,
-        false,
-        false,
-        true,
-    },
-    &[_]bool{
-        false,
-        true,
-        true,
-    },
-    &[_]bool{
-        true,
-        false,
-        false,
-        true,
-    },
-    &[_]bool{
-        true,
-        false,
-        true,
-        true,
-    },
-    &[_]bool{
-        true,
-        true,
-        false,
-        false,
-    },
-};
+fn morseStringToBoolArray(s: []const u8) []bool {
+    var result: [s.len]bool = undefined;
+    for (s, 0..) |letter, i| {
+        var symbol: bool = blk: {
+            if (letter == "-"[0]) {
+                break :blk true;
+            } else if (letter == "."[0]) {
+                break :blk false;
+            } else {
+                unreachable;
+            }
+        };
+        result[i] = symbol;
+    }
+    return &result;
+}
+
+fn generateMorsePattern(s: []const []const u8) []const []const bool {
+    var result: [s.len][]const bool = undefined;
+    for (s, 0..) |string, i| {
+        result[i] = morseStringToBoolArray(string);
+    }
+    return &result;
+}
+
+const MorseStrings: []const []const u8 = &[_][]const u8{
+".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+"..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+"--.-", ".-.", "...", "-", "..-", "...-", "--.", "-..-",
+"-.--", "..--" };
+const M: []const []const bool = generateMorsePattern(MorseStrings);
